a39fb79d4baddc23817cd9b60ed834bd1ca88b7d
Tarini Sudhakar <tarinisudhakar@wireless-10-147-25-205.public.utexas.edu>
2022-12-01 16:22:04 -0600

    Just making sure this is working

--- code/get_sentiment.py
+++ code/get_sentiment.py
@@ -1,1 +1,9 @@
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+#doc = nlp("BMan I can't wait to hear all the crazy laws Fetterman will pass according to my Fox News loving coworkers.  Edit: Holy shit didn't think this would blow up. Thank you and apologies for not responding to comments.")
+#doc2 = nlp("YES THANK YOU PENNSYLVANIA. You all showed up!! Fetterman will be my favorite Senator in Congress now. Oz can move back to New Jersey and hopefully fade away into obscurity now.")
+
+print([(X.text, X.label_) for X in doc2.ents])
 
