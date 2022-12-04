fe2d24c6d0172b4fc3d8a53f4eb7eea4379c75af
Tarini Sudhakar <tarinisudhakar@wireless-10-145-29-154.public.utexas.edu>
2022-12-04 15:53:18 -0600

    For loop is broken

--- code/get_sentiment.py
+++ code/get_sentiment.py
@@ -16,45 +16,54 @@ group by
 """
 
 df = pd.read_sql_query(text, engine)
+#print(df)
 
 df['comment'] = df['comment'].astype(str)
 
 sample = df.sort_values(by=['comment']).head(5)
-print(sample)
+#print(sample)
 
-article = article = [_ for _ in sample['comment']]
-
-entities_by_article = []
-dic = defaultdict(list)
+article = [_ for _ in sample['comment']]
+print(article)
+#print(type(article))
 
 for a in article:
-	doc = nlp(a)
-	people = []
-	org = []
-	for ent in doc.ents:
-		if ent.label_ == "PERSON":
-			people.append(ent)
-		if ent.label_ == "ORG": 
-			org.append(ent)
-	dic['People'].append(people)
-	dic['Organisation'].append(org)
-	entities_by_article.append(dic)
+ 	doc = nlp(a)
+#print(a)
+#print(a)
 
-print(entities_by_article)
+# entities_by_article = []
+# dic = defaultdict(list)
 
-check = pd.DataFrame(entities_by_article)
+# for a in article:
+# 	doc = nlp(a)
+# 	people = []
+# 	org = []
+# 	for ent in doc.ents:
+# 		if ent.label_ == "PERSON":
+# 			people.append(ent)
+# 		if ent.label_ == "ORG": 
+# 			org.append(ent)
+# 	dic['People'].append(people)
+# 	dic['Organisation'].append(org)
+# 	entities_by_article.append(dic)
 
-print(check)
+# print(entities_by_article)
 
+# check = pd.DataFrame(entities_by_article)
+# check['People'] = check['People'].astype(str)
+# check['Organisation'] = check['Organisation'].astype(str)
+
+# print(check)
+# print(check.dtypes)
 
 # insert_entity = """
 # insert into reddit_comments (people, organisation)
 # VALUES (%(People)s, %(Organisation)s); 
 # """
 
-with engine.connect() as connection:
-	#connection.exec_driver_sql(insert_entity, check)
-	check.to_sql('reddit_comments', con=connection, if_exists='append')
+# with engine.connect() as connection:
+# 	check.to_sql('reddit_comments', con=connection, if_exists='append',index=False)
 
 
 
