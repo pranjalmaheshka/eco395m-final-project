71b1074dadd3043da1d484164d552744b48e9817
Tarini Sudhakar <tarinisudhakar@wireless-10-145-29-154.public.utexas.edu>
2022-12-04 14:39:43 -0600

    Need to fix type error

--- code/get_sentiment.py
+++ code/get_sentiment.py
@@ -47,24 +47,14 @@ check = pd.DataFrame(entities_by_article)
 print(check)
 
 
-insert_entity = """
-ALTER TABLE checkdata 
-ADD people, organisation varchar;
-"""
+# insert_entity = """
+# insert into reddit_comments (people, organisation)
+# VALUES (%(People)s, %(Organisation)s); 
+# """
 
 with engine.connect() as connection:
-	check.to_sql('checkdata', con=connection, if_exists='append')
+	#connection.exec_driver_sql(insert_entity, check)
+	check.to_sql('reddit_comments', con=connection, if_exists='append')
 
-#for row in range(len(df)): 
-#	doc = nlp(df.loc[row, 'comments']) 
 
-# for i in doc.ents:  
-# 	print[(i.text, i.label_)]
-
-#print([(i.text, i.label_) for i in doc.ents])
-#doc = nlp("BMan I can't wait to hear all the crazy laws Fetterman will pass according to my Fox News loving coworkers.  Edit: Holy shit didn't think this would blow up. Thank you and apologies for not responding to comments.")
-#doc2 = nlp("YES THANK YOU PENNSYLVANIA. You all showed up!! Fetterman will be my favorite Senator in Congress now. Oz can move back to New Jersey and hopefully fade away into obscurity now.")
-
-#print(test)
-#print([(X.text, X.label_) for X in doc2.ents])
 
