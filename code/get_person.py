538981cf0a1014e2d7439e151829d88718b589f4
Tarini Sudhakar <tarinisudhakar@Tarinis-MacBook-Air-2.local>
2022-12-04 21:11:41 -0600

    Updated analysis

--- code/get_person.py
+++ code/get_person.py
@@ -1,23 +1,25 @@
 from database import engine 
 from collections import Counter
 from collections import defaultdict
+from get_token import tokens
 import pandas as pd
 import spacy
 
-'''Entity recognition for Reddit comments'''
 nlp = spacy.load("en_core_web_sm")
 nlp.max_length = 5000000
-text = """
+
+'''Entity recognition for Reddit comments'''
+reddit_comments = """
 select 
     reddit_post_id, 
     comment
 from 
     reddit_comments rc
 group by 
-    rc.reddit_post_id, rc.comment
+    rc.reddit_post_id, rc.comment limit 50 
 """
 
-df = pd.read_sql_query(text, engine)
+df = pd.read_sql_query(reddit_comments, engine)
 
 df['comment'] = df['comment'].astype(str)
 
@@ -38,23 +40,43 @@ print(len(df['people']), df)
 
 
 with engine.connect() as connection:
-  df.to_sql('temp_entity', con=connection, if_exists='replace',index=False)
+  df.to_sql('temp_red_comm_entity', con=connection, if_exists='replace',index=False)
 
 
-sql = """
+reddit_comment_sql = """
     UPDATE reddit_comments AS f
     SET people = t.people
-    FROM temp_entity AS t
+    FROM temp_red_comm_entity AS t
     WHERE f.reddit_post_id = t.reddit_post_id
 """
 
 with engine.begin() as conn:     # TRANSACTION
-    conn.execute(sql)
+    conn.execute(reddit_comment_sql)
 
-'''Analysis'''
+'''Entity recognition for Reddit posts'''
+
+'''Entity recognition for Tweets''' 
 
 
-# person_counts = Counter(person_list).most_common(50)
-# df_person = pd.DataFrame(person_counts, columns =['Person', 'Count'])
+'''Analysis: top entities'''
 
-# print('This is person_list', len(person_list), person_list)
+person_list = []
+for ent in tokens.ents:
+    if ent.label_ == 'PERSON':
+        person_list.append(ent.text)
+        
+person_counts = Counter(person_list).most_common(20)
+df_person = pd.DataFrame(person_counts, columns =['text', 'count'])
+
+print('Length of df_person', len(df_person), df_person)
+
+'''Analysis: top nationalities, religious, and political groups'''
+norp_list = []
+for ent in tokens.ents:
+    if ent.label_ == 'NORP':
+        norp_list.append(ent.text)
+        
+norp_counts = Counter(norp_list).most_common(20)
+df_norp = pd.DataFrame(norp_counts, columns =['text', 'count'])
+
+print('Length of df_norp', len(df_norp), df_norp)
