b930c090fa008551aa66a089a43c11d66fdbbf9b
Pranjal Maheshka <pranjalmaheshka@gmail.com>
2022-12-04 20:47:43 -0600

    update nlp

--- code/get_sentiment.py
+++ code/get_sentiment.py
@@ -1,31 +1,30 @@
+from database import engine
 import pandas as pd
 from nltk.sentiment import SentimentIntensityAnalyzer
 sia = SentimentIntensityAnalyzer()
 
-reddit_comments = pd.read_csv("reddit_comment_results.csv")
+import matplotlib
+import time
+start = time.time()
+
+#reddit_comments = pd.read_csv("reddit_comment_results.csv")
 
 """Querying reddit posts from SQL"""
 posts = """
-select * from reddit_posts rp 
+select * from reddit_posts rp limit 100
 """
 posts_df = pd.read_sql_query(posts, engine)
+#print(posts_df)
 
 """Querying reddit comments from SQL"""
 comments = """
-select * from reddit_comments rc 
+select * from reddit_comments rc limit 100
 """
-comments_df = pd.read_sql_query(comments, engine)
+reddit_comments = pd.read_sql_query(comments, engine)
 
-
-reddit_comments['score_neg'] = 0
-reddit_comments['score_neu'] = 0
-reddit_comments['score_pos'] = 0
-reddit_comments['score_compound'] = 0
-reddit_comments['score'] = 0
 # Vader: 1 is neg, 2 is neu, 3 is pos 
-
 for ind in reddit_comments.index:
-    score = sia.polarity_scores(reddit_comments.loc[ind][2])
+    score = sia.polarity_scores(reddit_comments.loc[ind]["comment"])
     reddit_comments.at[ind,'score_neg'] = score["neg"]
     reddit_comments.at[ind,'score_neu'] = score["neu"]
     reddit_comments.at[ind,'score_pos'] = score["pos"]
@@ -50,3 +49,22 @@ for ind in reddit_comments.index:
                 reddit_comments.at[ind,'score'] = 4
     else:
         reddit_comments.at[ind,'score'] = 100
+
+upvotes_series = reddit_comments.groupby(['score'])['upvotes'].sum()
+sentiments_df = reddit_comments.groupby(['score'])['score'].count()
+sentiments = ["Very Negative", "Negative", "Neutral", "Positive", "Very Positive"]
+print(upvotes_series)
+print(sentiments_df)
+#print(type(sentiments_df))
+
+votes_by_sentiment = upvotes_series/sentiments_df
+results = pd.concat([sentiments, upvotes_series, sentiments_df,votes_by_sentiment], axis=1)
+results.columns =["Sentiment", "Sentiment Count", "Total Upvotes", "Avg Upvotes"]
+print(results)
+print(results.hist)
+
+end = time.time()
+ttime = end - start
+print("Runtime is " + str(ttime))
+
+reddit_comments.to_csv("reddit_comments_pranjal.csv")
