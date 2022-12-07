5630cfe17b88a30f2fdb83647466fb0e8fe686b8
Annie Nguyen <annienguyen879@gmail.com>
2022-12-06 18:34:23 -0600

    updating reddit_scrape in main

--- code/main.py
+++ code/main.py
@@ -1,20 +1,20 @@
 ## Main File
 import os
 import pandas as pd
-from get_reddit import X
+from get_reddit import scrape_reddit
 from get_twitter import X
 from get_sentiment import get_sentiment
 from get_entity import get_entity
 
 # Step 1: Make sure GCP database instance is configured
-get_reddit()
+scrape_reddit(50)
 get_twitter()
 
 # Step 2: Upload csv output in eco395m-final-project/artifacts
 get_sentiment(?)
 get_entity(?)
 
-# Step 3: Data Visualization 
+# Step 3: Data Visualization
 
 
 if __name__ == "__main__":
