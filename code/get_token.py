489910beb29a51153003a7b340357f2891d3c848
Tarini Sudhakar <35040985+tarinisudhakar@users.noreply.github.com>
2022-12-07 23:39:44 -0600

    Update get_token.py

--- code/get_token.py
+++ code/get_token.py
@@ -18,21 +18,23 @@ group by
     rc.reddit_post_id, rc.comment, rc.id
 """
 
-red_df = pd.read_sql_query(reddit_comments, engine)
+def get_reddit_tokens(): 
+    red_df = pd.read_sql_query(reddit_comments, engine)
 
-red_df['comment'] = red_df['comment'].astype(str)
+    red_df['comment'] = red_df['comment'].astype(str)
 
-red_df['tokenized'] = red_df['comment'].apply(lambda x: nlp(x))
+    red_df['tokenized'] = red_df['comment'].apply(lambda x: nlp(x))
+    
+    red_df['tokenized'] = red_df['tokenized'].astype(str)
 
-#red_df['lemmatized'] = red_df['tokenized'].apply(lambda x: [y.lemma_ for y in nlp(x)])
+    red_tokens = nlp(''.join(str(red_df.tokenized.tolist())))
 
-red_tokens = nlp(''.join(str(red_df.tokenized.tolist())))
+    red_items = [x.text for x in red_tokens.ents]
 
-red_items = [x.text for x in red_tokens.ents]
+    red = Counter(red_items).most_common(50)
 
-red = Counter(red_items).most_common(50)
+    red_tokens_df = pd.DataFrame.from_records(red, columns=['Token','Count'])
 
-red_tokens_df = pd.DataFrame.from_records(red, columns=['Token','Count'])
 
 print('Length of Reddit tokens', len(red_tokens_df), red_tokens_df)
 
@@ -49,21 +51,22 @@ group by
     tc.reddit_post_id, tc.tweet, tc.id
 """
 
-tweet_df = pd.read_sql_query(twitter_comments, engine)
+def get_twitter_tokens(): 
+    tweet_df = pd.read_sql_query(twitter_comments, engine)
 
-tweet_df['tweet'] = tweet_df['tweet'].astype(str)
+    tweet_df['tweet'] = tweet_df['tweet'].astype(str)
 
-tweet_df['tokenized'] = tweet_df['tweet'].apply(lambda x: nlp(x))
+    tweet_df['tokenized'] = tweet_df['tweet'].apply(lambda x: nlp(x))
 
-#tweet_df['lemmatized'] = tweet_df['tokenized'].apply(lambda x: [y.lemma_ for y in nlp(x)])
+    tweet_df['tokenized'] = red_df['tokenized'].astype(str)
 
-twitter_tokens = nlp(''.join(str(tweet_df.tokenized.tolist())))
+    twitter_tokens = nlp(''.join(str(tweet_df.tokenized.tolist())))
 
-twitter_items = [x.text for x in twitter_tokens.ents]
+    twitter_items = [x.text for x in twitter_tokens.ents]
 
-twit = Counter(twitter_items).most_common(50)
+    twit = Counter(twitter_items).most_common(50)
 
-twit_tokens_df = pd.DataFrame.from_records(twit, columns=['Token','Count'])
+    twit_tokens_df = pd.DataFrame.from_records(twit, columns=['Token','Count'])
 
 print('Length of Twitter tokens', len(twit_tokens_df), twit_tokens_df)
 
@@ -79,21 +82,21 @@ group by
     tc.reddit_post_id, tc.user_desc, tc.id
 """
 
+def get_twituser_tokens(): 
+    desc_df = pd.read_sql_query(twitter_desc, engine)
 
-desc_df = pd.read_sql_query(twitter_desc, engine)
+    desc_df['user_desc'] = desc_df['user_desc'].astype(str)
 
-desc_df['user_desc'] = desc_df['user_desc'].astype(str)
+    desc_df['tokenized'] = desc_df['user_desc'].apply(lambda x: nlp(x))
 
-desc_df['tokenized'] = desc_df['user_desc'].apply(lambda x: nlp(x))
+    desc_df['tokenized'] = red_df['tokenized'].astype(str)
 
-#desc_df['lemmatized'] = desc_df['tokenized'].apply(lambda x: [y.lemma_ for y in nlp(x)])
+    desc_tokens = nlp(''.join(str(desc_df.tokenized.tolist())))
 
-desc_tokens = nlp(''.join(str(desc_df.tokenized.tolist())))
+    twitter_desc_items = [x.text for x in desc_tokens.ents]
 
-twitter_desc_items = [x.text for x in desc_tokens.ents]
+    desc = Counter(twitter_desc_items).most_common(50)
 
-desc = Counter(twitter_desc_items).most_common(50)
-
-desc_tokens_df = pd.DataFrame.from_records(desc, columns=['Token','Count'])
+    desc_tokens_df = pd.DataFrame.from_records(desc, columns=['Token','Count'])
 
 print('Length of Twitter tokens', len(desc_tokens_df), desc_tokens_df)
