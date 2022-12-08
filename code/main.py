413596f6d3bde03783fa33693c3f890917865d00
Tarini Sudhakar <35040985+tarinisudhakar@users.noreply.github.com>
2022-12-07 23:38:24 -0600

    Update main.py

--- code/main.py
+++ code/main.py
@@ -4,7 +4,19 @@ import pandas as pd
 from get_reddit import scrape_reddit
 from get_twitter import twitter_scraper
 from get_sentiment import get_sentiment
-from get_entity import get_entity
+from get_person import get_reddit_people
+from get_person import get_reddit_org
+from get_person import get_reddit_norp
+from get_person import get_twitter_people
+from get_person import get_twitter_org
+from get_person import get_twitter_norp
+from get_person import get_twitter_norp
+from get_person import get_user_org
+from get_person import get_user_norp
+from get_person import upload_reddit_entity
+from get_person import upload_twitter_entity
+from get_person import upload_user_entity
+
 
 # Step 1: Make sure GCP database instance is configured
 scrape_reddit(50)
@@ -13,6 +25,20 @@ twitter_scraper()
 # Step 2: Upload csv output in eco395m-final-project/artifacts
 get_sentiment(?)
 get_entity(?)
+
+#Steo ?: Run get_person to analyse data and updata database with recognised entities
+get_reddit_people()
+get_reddit_org()
+get_reddit_norp()
+get_twitter_people()
+get_twitter_org()
+get_twitter_norp()
+get_twitter_norp()
+get_user_org()
+get_user_norp()
+upload_reddit_entity()
+upload_twitter_entity()
+upload_user_entity()
 
 # Step 3: Data Visualization
 
